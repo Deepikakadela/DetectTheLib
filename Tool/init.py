bf5e4bb1f0241d8ef7eab7bcede0cf07ee1d39eb
Deepikakadela <tcs15b011@iittp.ac.in>
2018-03-18 00:47:28 +0530

    Create init.py

---
+++ Tool/init.py
@@ -0,0 +1,15 @@
+from libradar import LibRadar
+
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "libradar"
+__summary__ = "A tool for Android library detection."
+__uri__ = "https://github.com/pkumza/libradar"
+
+__version__ = "2.2.1"
+
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2017 " + __author__
