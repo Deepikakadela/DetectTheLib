c3bcc9d3c7e19f3591da3df7a404b350bfb6e805
Deepikakadela <tcs15b011@iittp.ac.in>
2018-03-20 10:46:25 +0530

    Update tool.py

--- Tool/tool.py
+++ Tool/tool.py
@@ -109,6 +109,46 @@ class LibRadar(object):
                     for permission in self.k_api_v_permission[decoded_instruction.getApi][0]:
                         permission_list.add(permission)
         return
+     #compute the Hash value of each method
+            methodmd5 = hashlib.md5()
+            methodmd5.update(method_opcode_str)
+            method_md5_index =methodmd5.hexdigest()
+
+            method_name_list.append(filename)
+            set_invoke_info = set(invoke_info)
+            method_info.append(set_invoke_info)
+            method_info.append(method_md5_index)
+            
+            method_dict[filename] = method_info
+            
+    return method_dict, classes_list       
+   
+#compute the HASH Value of the classes in classes_list
+#using the classes_list, method_dict, and method_name_list
+def class_info_index(classes_list, method_dict):
+    classes_info_dict = {}
+    classcount = 0
+    method_list = method_dict.keys()
+    print "method_list", method_list
+    for cls_nc_pair in classes_list:
+        class_info = []
+        class_cfg = ""
+        out_list = []
+        classname = cls_nc_pair[0]
+        class_invoke_set = set()
+        for method in method_list:
+            method_info = method_dict[method]
+            print method.count(classname)
+            if method.count(classname[1:]) == 1:
+                method_hash = method_info[1]
+                print method_hash
+                out_list.append(method_hash)
+                
+                method_invoke_set = method_info[0]
+                class_invoke_set.update(method_invoke_set)
+                
+        out_list.sort()
+
 
     def extract_class(self, dex_class_def_obj):
         class_sha256 = hashlib.sha256()
